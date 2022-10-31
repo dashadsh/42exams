7e28e59c7b587ec691b6844eae64d7c66f86459d
Daria Goremykina <d.goremykina@gmail.com>
2022-10-31 20:26:45 +0100

    m

--- rot_13.c
+++ rot_13.c
@@ -26,3 +26,41 @@
 // $>./rot_13 "" | cat -e
 // $
 // $>
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int c;
+	int i = 0;
+
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z' || 
+				av[1][i] >= 'a' && av[1][i] <= 'z')
+				{
+				if (av[1][i] >= 'A' && av[1][i] <= 'M' || 
+					av[1][i] >= 'a' && av[1][i] <= 'm')
+					c = av[1][i] + 13;
+				else if (av[1][i] >= 'N' && av[1][i] <= 'Z' || 
+					av[1][i] >= 'n' && av[1][i] <= 'z')
+					c = av[1][i] - 13;
+				write(1, &c, 1);
+				}
+			else
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+// 97a-> 110n
+// 122z-> 109m
+
+// 110 97
+// 111 98
--- rotone.c
+++ rotone.c
@@ -18,7 +18,7 @@
 // bcd
 // $>./rotone "Les stagiaires du staff ne sentent pas toujours tres bon." | cat -e
 // Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
-// $>./rotone "AkjhZ zLKIJz , 23y " | cat -e
+// $>./rotone./| cat -e
 // BlkiA aMLJKa , 23z $
 // $>./rotone | cat -e
 // $
@@ -26,3 +26,31 @@
 // $>./rotone "" | cat -e
 // $
 // $>
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int c;
+	int i = 0;
+
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{	
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z' || av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+				if (av[1][i] == 'Z' || av[1][i] == 'z')
+					c = av[1][i] - 25;
+				else
+					c = av[1][i] + 1;
+				write(1, &c, 1);
+			}
+			else
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
