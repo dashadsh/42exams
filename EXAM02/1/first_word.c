c3b5d818fbb05ad193250fe6e8a91b95bbef8120
Daria Goremykina <d.goremykina@gmail.com>
2022-10-31 15:19:11 +0100

    .

---
+++ EXAM02/1/first_word.c
@@ -0,0 +1,54 @@
+// Assignment name  : first_word
+// Expected files   : first_word.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string and displays its first word, followed by a
+// newline.
+
+// A word is a section of string delimited by spaces/tabs or by the start/end of
+// the string.
+
+// If the number of parameters is not 1, or if there are no words, simply display
+// a newline.
+
+// Examples:
+
+// $> ./first_word "FOR PONY" | cat -e
+// FOR$
+// $> ./first_word "this        ...    is sparta, then again, maybe    not" | cat -e
+// this$
+// $> ./first_word "   " | cat -e
+// $
+// $> ./first_word "a" "b" | cat -e
+// $
+// $> ./first_word "  lorem,ipsum  " | cat -e
+// lorem,ipsum$
+// $>
+
+#include <unistd.h>
+
+int separator(char c)
+{
+	if (c == '\t' || c == '\n' || c =='\v' || c == '\f' || c =='\r' || c == ' ')
+		return (1);
+	return (0);
+}
+int main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (separator(av[1][i]))
+			i++; //if it starts with spaces
+		while (av[1][i] && !(separator(av[1][i])))
+		{
+			write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return(0);
+}
